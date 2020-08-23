6f91059bc1d7c0bb84bab62d2e4207ef18a34406
balajich <balaji.chopparapu@gmail.com>
2020-08-23 23:27:04 +0530

    initial

--- gateway/src/main/java/org/eduami/spring/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/eduami/spring/gateway/GatewayApplication.java
@@ -2,12 +2,10 @@ package org.eduami.spring.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
